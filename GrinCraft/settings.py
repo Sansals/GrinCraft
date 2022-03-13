af2e66a41cda2ba99145c71e868e521f9fd8e3fd
Vitaliy Grigorev <96786249+Sansals@users.noreply.github.com>
2022-03-13 16:54:11 +0700

    Update settings.py

--- GrinCraft/settings.py
+++ GrinCraft/settings.py
@@ -126,7 +126,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465 #2525
 EMAIL_HOST_USER = "stereotip.228@gmail.com"
-EMAIL_HOST_PASSWORD = "Rjnbr.2016"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
